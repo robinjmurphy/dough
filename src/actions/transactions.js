301344c29a776eabead73d85cbbe192456f169f2
Robin Murphy <robin@robinmurphy.co.uk>
2016-11-04 20:46:22 +0000

    Remove unused action

--- src/actions/transactions.js
+++ src/actions/transactions.js
@@ -4,7 +4,6 @@ import Transactions from '../models/Transactions';
 
 export const REQUEST_TRANSACTIONS = 'REQUEST_TRANSACTIONS';
 export const RECEIVE_TRANSACTIONS = 'RECEIVE_TRANSACTIONS';
-export const RECEIVE_TRANSACTIONS_FAILURE = 'RECEIVE_TRANSACTIONS_FAILURE';
 
 export const requestTransactions = createAction(REQUEST_TRANSACTIONS);
 export const receiveTransactions = createAction(RECEIVE_TRANSACTIONS);
